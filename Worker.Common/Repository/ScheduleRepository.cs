660985ee1f5b9299d6bab9f2412a89465611e78b
tronelius <per.tronelius@gmail.com>
2015-09-28 08:32:12 +0200

    moved Interfaces to own files

--- Worker.Common/Repository/ScheduleRepository.cs
+++ Worker.Common/Repository/ScheduleRepository.cs
@@ -6,16 +6,6 @@ using Worker.Common.Model;
 
 namespace Worker.Common.Repository
 {
-    public interface IScheduleRepository
-    {
-        Schedule Get(int id);
-        int Insert(Schedule schedule);
-        List<Schedule> List();
-        void Update(Schedule schedule);
-        void Delete(int id);
-        bool IsInUse(int id);
-    }
-
     public class ScheduleRepository : BaseRepository, IScheduleRepository
     {
         public ScheduleRepository(string connectionstring) : base(connectionstring){}
--- Worker.Common/Repository/SubscriptionRepository.cs
+++ Worker.Common/Repository/SubscriptionRepository.cs
@@ -8,18 +8,6 @@ using Worker.Common.Model;
 
 namespace Worker.Common.Repository
 {
-    public interface ISubscriptionRepository
-    {
-        Subscription Get(int id);
-        int Insert(Subscription schedule);
-        List<Subscription> List();
-        void Update(Subscription schedule);
-        void Delete(int id);
-        void UpdateTemplateText(UpdateTemplateText updateTemplateText);
-        void SendNow(int id);
-        List<Subscription> GetSubscriptionsWithSendDateBefore(DateTime now, int maxFailed);
-    }
-
     public class SubscriptionRepository : BaseRepository, ISubscriptionRepository
     {
         public SubscriptionRepository(string connectionstring) : base(connectionstring){}
