58feb9441fbec5b7da9c353a993b3bea6bd0446b
tronelius <per.tronelius@gmail.com>
2016-04-12 15:08:01 +0200

    SQL folder ignored before

---
+++ SimpleReport.Model/Storage/SQL/DapperMappings.cs
@@ -0,0 +1,73 @@
+﻿using System.Management.Instrumentation;
+using DapperExtensions.Mapper;
+
+namespace SimpleReport.Model.Storage.SQL
+{
+    public class ReportMap : ClassMapper<Report>
+    {
+        public ReportMap()
+        {
+            Map(a => a.Id).Key(KeyType.Assigned);
+            Map(a => a.Parameters).Ignore();
+            AutoMap();
+        }
+    }
+
+    public class ConnectionMap : ClassMapper<Connection>
+    {
+        public ConnectionMap()
+        {
+            Map(a => a.Id).Key(KeyType.Assigned);
+            AutoMap();
+        }
+    }
+
+    public class ParameterMap : ClassMapper<Parameter>
+    {
+        public ParameterMap()
+        {
+            Map(a => a.SqlKey).Key(KeyType.Assigned);
+            Map(a => a.ReportId).Key(KeyType.Assigned);
+            Map(a => a.Choices).Ignore();
+            Map(a => a.Key).Ignore();
+            AutoMap();
+        }
+    }
+
+    public class LookupReportMap : ClassMapper<LookupReport>
+    {
+        public LookupReportMap()
+        {
+            Map(a => a.Id).Key(KeyType.Assigned);
+            Map(a => a.Group).Ignore();
+            Map(a => a.Description).Ignore();
+            Map(a => a.AccessId).Ignore();
+            Map(a => a.ReportOwnerAccessId).Ignore();
+            AutoMap();
+        }
+    }
+
+
+    public class TypeAheadReportMap : ClassMapper<TypeAheadReport>
+    {
+        public TypeAheadReportMap()
+        {
+            Map(a => a.Id).Key(KeyType.Assigned);
+            Map(a => a.Group).Ignore();
+            Map(a => a.Description).Ignore();
+            Map(a => a.AccessId).Ignore();
+            Map(a => a.ReportOwnerAccessId).Ignore();
+            AutoMap();
+        }
+    }
+
+    public class AccessMap : ClassMapper<Access>
+    {
+        public AccessMap()
+        {
+            Map(a => a.Id).Key(KeyType.Assigned);
+            Map(a => a.SplittedAdGroups).Ignore();
+            AutoMap();
+        }
+    }
+}
---
+++ SimpleReport.Model/Storage/SQL/SQLSetting.cs
@@ -0,0 +1,8 @@
+namespace SimpleReport.Model.Storage.SQL
+{
+    public class SQLSetting
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
