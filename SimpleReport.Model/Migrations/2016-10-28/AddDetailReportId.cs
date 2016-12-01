91ac1ef2961f4723a07e31d7420a95b4d808a333
tronelius <per.tronelius@gmail.com>
2016-12-01 11:43:47 +0100

    Added missing migration

---
+++ SimpleReport.Model/Migrations/2016-10-28/AddDetailReportId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+
+namespace SimpleReport.Model.Migrations
+{
+    [Migration(201610281413)]
+    public class AddDetailReportIdToReport : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Report").AddColumn("DetailReportId").AsGuid().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("DetailReportId").FromTable("Report");
+        }
+    }
+}
+
