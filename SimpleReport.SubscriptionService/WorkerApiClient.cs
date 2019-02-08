11b30ad6eaa64d672a4ddab847d1e210ef7996ac
martinsjolanders@hotmail.com <martinsjolanders@hotmail.com>
2019-02-08 10:26:01 +0100

    Fix for headers

--- SimpleReport.SubscriptionService/WorkerApiClient.cs
+++ SimpleReport.SubscriptionService/WorkerApiClient.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Web;
 using SimpleReport.Model.Subscriptions;
 
 namespace SimpleReport.SubscriptionService
@@ -21,11 +22,20 @@ namespace SimpleReport.SubscriptionService
                 var result = client.DownloadData(new Uri(client.BaseAddress + "Home/ExecuteReport/?" + parameters));
                 if (result.Length == 0) //only look for file if we have any result...
                     return null;
-
-                var headers = ContentDispositionHeaderValue.Parse(client.ResponseHeaders.Get("Content-Disposition"));
+                
+                var headerString = FixHeaderString(client.ResponseHeaders.Get("Content-Disposition"));
+                var headers = ContentDispositionHeaderValue.Parse(headerString);
                 return new ReportResult(result, headers.FileName.Replace("\"", ""));
                 
             }
+        }
+
+        // Quickfix to fix header, for some reason it seems that the name gets wrong chars in it, and decoding it and decoding it back seems to fix it.
+        private string FixHeaderString(string headerString)
+        {
+            headerString = HttpUtility.UrlDecode(headerString);
+            headerString = HttpUtility.UrlEncode(headerString);
+            return headerString;
         }
 
         //we currently think this is good enough.
