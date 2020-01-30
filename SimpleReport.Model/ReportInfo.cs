2b6855db07f89fa3462bda6730ac42c9c3b691f6
Elin Ohlsson <ElinO@activesolution.se>
2020-01-30 11:28:44 +0100

    Bugfix.

--- SimpleReport.Model/ReportInfo.cs
+++ SimpleReport.Model/ReportInfo.cs
@@ -26,8 +26,6 @@ namespace SimpleReport.Model
         [NonSerialized]
         public Access ReportOwnerAccess; //TODO rename to reportowner
 
-        public bool MatchSearchResult { get; set; } = true;
-
         public bool IsAvailableForMe(IPrincipal user, Access adminAccess)
         {
             return (ReportOwnerAccess != null && ReportOwnerAccess.IsAvailableForMe(user)) || (Access == null || Access.IsAvailableForMe(user) || adminAccess.IsAvailableForMe(user));
--- SimpleReport/Scripts/app/search/search-controller.js
+++ SimpleReport/Scripts/app/search/search-controller.js
@@ -1,35 +1,61 @@
 ﻿angular.module('search').controller('searchController', ['$scope', 'viewModel', function ($scope, viewModel) {
 
-    $scope.searchValue = null;
-
+    $scope.searchValue = "";
+    
     $scope.init = function () {
+        $scope.searchResult = [];
         $scope.search = search;
         $scope.viewModel = viewModel;
         $scope.isVisible = isVisible;
         $scope.showReport = showReport;
         $scope.visibleGroups = visibleGroups;
         $scope.reportsMissingGroup = reportsMissingGroup;
-
+        $scope.matchSearchValue = matchSearchValue;
+        $scope.inSearchResult = inSearchResult;
+        $scope.searchInProgress = searchInProgress;
     };
     $scope.init();
 
     function search(value) {
+        searchValue = value;
+        searchResult = [];
         let groups = viewModel.ReportGroups;
         for (let i = 0; i < groups.length; i++) {
             let group = groups[i];
             for (let j = 0; j < group.length; j++) {
-
                 let report = group[j];
-                let match = report.Name.toLowerCase().indexOf(value.toLowerCase()) !== -1;
-                report.MatchSearchResult = match;
+                if (matchSearchValue(report)) {
+                    searchResult.push(report.Id);
+                }
             }
         }
+        console.log(searchResult);
+    }
+
+    function matchSearchValue(report) {
+        return report.Name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1;
+    }
+
+    function searchInProgress() {
+        return $scope.searchValue !== null && $scope.searchValue.length > 0;
+    }
+
+    function inSearchResult(report) {
+        if (!searchInProgress()) {
+            return true;
+        }
+
+        return searchResult.indexOf(report.Id) !== -1;
     }
 
     function isVisible(group) {
+        if (!searchInProgress()) {
+            return true;
+        }
+
         for (let i = 0; i < group.length; i++) {
             let report = group[i];
-            if (report.MatchSearchResult) {
+            if (inSearchResult(report)) {
                 return true;
             }
         }
@@ -71,7 +97,6 @@
                 }
             }
         }
-
         return reports;
     }
 
